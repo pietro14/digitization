d490b90b09fc606a57e89e26ea49043e01942aee
Boba Fett <diclemente.1592703@studenti.uniroma1.it>
2019-11-13 13:16:16 +0100

    option changes

--- MC_data_gen.py
+++ MC_data_gen.py
@@ -58,12 +58,10 @@ if __name__ == "__main__":
 
 	parser = optparse.OptionParser("usage: %prog [options] arg1 arg2")
 
-	parser.add_option("-I", "--inputfolder", dest="infolder", default=os.getcwd()+"src", help="specify the folder containing input files")
-	parser.add_option("-O", "--outputfolder", dest="outfolder", defaul=os.getcwd()+"out", help="specify the output destination folder")
-	parser.add_option("-F", "--run_number_file", dest="runs", default="MC_runs.txt", help="specify the .txt file in which the last number of run used is")
-	parser.add_option("-S", "--set_run_number", dest="def_Nrun", default=None, help="manually sets the run number")	
-
-	(opt, args) = parser.parse_args()
+	parser.add_option("-I", "--inputfolder", dest="infolder", default=os.getcwd()+"/src", help="specify the folder containing input files")
+	parser.add_option("-O", "--outputfolder", dest="outfolder", default=os.getcwd()+"/out", help="specify the output destination folder")
+	
+        (opt, args) = parser.parse_args()
 
 	config = open(args[0], "r") 		#GET CONFIG FILE
 	params = eval(config.read()) 		#READ CONFIG FILE
@@ -73,40 +71,36 @@ if __name__ == "__main__":
 
 #### CODE EXECUTION ####
 
+        run_count=90000
+
+        print os.listdir(os.getcwd())
+
+        if not os.path.exists(opt.outfolder):
+            os.makedirs(opt.outfolder)
+            
+        print os.listdir(os.getcwd())
+
 	for infile in os.listdir(opt.infolder):
 		
 		if infile.endswith('.root'):		
-			if opt.def_Nrun == None:
-
-	                	run_number = open(opt.runs, "r")        ## GET INCREMENTAL NUMBER OF FILE
-        	        	Nrun=run_number.read()                  ## READ LAST RUN NUMBER
-
-                		run_number = open(opt.runs, "w")
-                		run_number.write(str(int(Nrun)+1))      ## WRITE ON FILE NEW RUN NUMBER
-                		run_number.close()                      ## CLOSE THE FILE
-
-        		else:
-
-                		Nrun=opt.def_Nrun
-
 
 			rootfile=TFile.Open(opt.infolder+infile)
 			tree=rootfile.Get('nTuple')
-			outfile=TFile('out/histograms_Run'+Nrun+'.root', 'RECREATE')
+			outfile=TFile(opt.outfolder+'/histograms_Run'+str(run_count)+'.root', 'RECREATE')
                		SaveValues(params, outfile) ## SAVE PARAMETER OF THE RUN
 
 			final_imgs=list(); nobckg_imgs=list()
 
-			for num in range(0, tree.GetEntries()-9998):
+			for num in range(0, tree.GetEntries()-8):
 			
 				final_imgs.append(TH2F()) ## HISTOGRAMS LIST SETUP
 			
-			for entry in range(0, tree.GetEntries()-9998):
+			for entry in range(0, tree.GetEntries()-8):
 		
 				print('number of tracks processed: %d'%(entry+1))
 				tree.GetEntry(entry)
 
-				final_imgs[entry]=TH2F('pic_run'+Nrun+'_ev'+str(entry), '', opt.z_pix, -opt.z_dim*0.5, opt.z_dim*0.5, opt.y_pix, -opt.y_dim*0.5, opt.y_dim*0.5) #smeared track with background
+				final_imgs[entry]=TH2F('pic_run'+str(run_count)+'_ev'+str(entry), '', opt.z_pix, -opt.z_dim*0.5, opt.z_dim*0.5, opt.y_pix, -opt.y_dim*0.5, opt.y_dim*0.5) #smeared track with background
 
 				AddTrack(tree, final_imgs, smearing)
 
@@ -115,8 +109,9 @@ if __name__ == "__main__":
 				
 		## WRITE EACH TRACK ON THE ROOT FILE CORRESPONDING TO THE ENERGY
 
-				final_imgs[entry].Write()	
+				final_imgs[entry].Write()
 
+                        run_count+=1
 			outfile.Close()
 
 
